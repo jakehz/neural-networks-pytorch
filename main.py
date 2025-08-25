1b6c6627c76b5f9475a265f41809fb2805598995
Jake Hernandez <jakejdhz@gmail.com>
2025-08-24 18:17:10 -0700

    Plot some visuals from FashionMNIST

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
 def main():
     training_data = datasets.FashionMNIST(
@@ -28,6 +29,19 @@ def main():
         print(f"Shape of X [N, C, H, W]: {X.shape}")
         print(f"Shape of y: {y.shape} {y.dtype}")
         break
+
+    figure = plt.figure(figsize=(8, 8))
+    cols, rows = 3, 3
+    for i in range(1, cols * rows + 1):
+        # returns a tensor of random integers from low (inclusive) to high (exclusive)
+        # size is the shape of the output tensor, here a single integer
+        sample_idx = torch.randint(len(test_data), size=(1,)).item() # Why did we use torch here?
+        img, label = test_data[sample_idx]
+        figure.add_subplot(rows, cols, i)
+        plt.title(label)
+        plt.axis("off")
+        plt.imshow(img.squeeze(), cmap="gray")
+    plt.show() 
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
