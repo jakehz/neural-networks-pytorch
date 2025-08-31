a6c639cdd46ab87f183c900622a861db9c317874
Jake Hernandez <jakejdhz@gmail.com>
2025-08-31 13:47:35 -0700

    Add display dataset function for debugging

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ import matplotlib
 matplotlib.use('TkAgg')  # Use TkAgg backend for matplotlib
 import matplotlib.pyplot as plt
 
+def display_dataset(d: DataLoader, figsize =(8,8), cols=4, rows=4):
+    figure = plt.figure(figsize=figsize)
+    for i in range(1, cols * rows + 1):
+        # returns a tensor of random integers from low (inclusive) to high (exclusive)
+        # size is the shape of the output tensor, here a single integer
+        sample_idx = torch.randint(len(d), size=(1,)).item() # Why did we use torch here?
+        img, label = d[sample_idx]
+        figure.add_subplot(rows, cols, i)
+        plt.title(label)
+        plt.axis("off")
+        plt.imshow(img.squeeze(), cmap="gray")
+    plt.show() 
+
+
 def main():
     training_data = datasets.FashionMNIST(
         root="data",
@@ -31,19 +45,8 @@ def main():
         print(f"Shape of X [N, C, H, W]: {X.shape}")
         print(f"Shape of y: {y.shape} {y.dtype}")
         break
-
-    figure = plt.figure(figsize=(8, 8))
-    cols, rows = 3, 3
-    for i in range(1, cols * rows + 1):
-        # returns a tensor of random integers from low (inclusive) to high (exclusive)
-        # size is the shape of the output tensor, here a single integer
-        sample_idx = torch.randint(len(test_data), size=(1,)).item() # Why did we use torch here?
-        img, label = test_data[sample_idx]
-        figure.add_subplot(rows, cols, i)
-        plt.title(label)
-        plt.axis("off")
-        plt.imshow(img.squeeze(), cmap="gray")
-    plt.show() 
+    
+    display_dataset(training_data)
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
